c310afd3d78e677c0a27c758855feb06dd834c87
manviupadhyay2 <manviupadhyay2@gmail.com>
2024-11-23 19:47:01 +0530

    changed layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Container } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
-
   return (
     <Container maxW={'none'} className="App" bgColor={'black'} bgGradient={'linear(to-l-r,black ,blue.900)'} color={'black'}>
       <ChatWithGemini />
