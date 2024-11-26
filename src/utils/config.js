ce5985a1c37ff88834a4c8f6013c7e43399edf77
manviupadhyay2 <manviupadhyay2@gmail.com>
2024-11-26 22:07:04 +0530

    modified

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,4 +1,4 @@
 const env = import.meta.env.MODE
 export const config = {
-    "API_KEY": env === "development" ? "" : import.meta.env.VITE_APP_BOT_API_KEY,
+    "API_KEY": env === "development" ? "" : import.meta.env.VITE_APP_BOT_API_KEY,//
 }
